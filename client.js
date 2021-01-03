1f4064ea6e752ed03caea5ee1d9d0d0756983e2f
Милош Ђелић <milos@djelic.info>
2021-01-03 19:56:16 +0100

    Adding project files

---
+++ client.js
@@ -0,0 +1,26 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader")
+const packageDef = protoLoader.loadSync("todo.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const text = process.argv[2];
+
+const client = new todoPackage.Todo("localhost:40000", 
+grpc.credentials.createInsecure())
+
+// id -1 will create a new ID
+client.createTodo({
+    "id": -1,
+    "text" : text
+}, (err, response) => {
+    console.log("Received from server " + JSON.stringify(response));
+});
+
+
+const call = client.readTodosStream();
+call.on("data",  item => {
+    console.log("received item from server ", JSON.stringify(item));
+});
+
+call.on("end", e => console.log('Server done'));
